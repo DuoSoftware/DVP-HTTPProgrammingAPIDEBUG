e7b8e9c74e2b6352b33041dfe6f68fcdbd4d16bf
Pawan Sasanka <pawan@duosoftware.com>
2015-05-06 09:32:07 +0530

    Config updated

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "DB": {
+        "Type":"DB_TYPE",
+        "User":"DB_USER",
+        "Password":"DB_PASSWORD",
+        "Port":"DB_PORT",
+        "Host":"DB_HOST",
+        "Database":"DB_BASE"
+    },
+
+
+    "Redis":
+    {
+        "ip": "REDIS_IP",
+        "port": "REDIS_PORT"
+
+    },
+    "Debug":{
+        "ip":"SYS_DBG_IP",
+        "port":"SYS_DBG_PORT"
+    },
+    "Host":
+    {
+        "domain": "HOST_IP",
+        "port": "HOST_PORT",
+        "version":"HOST_VERSION"
+    }
+};
+
+//NODE_CONFIG_DIR
---
+++ config/default.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+    "DB": {
+        "Type":"postgres",
+        "User":"duo",
+        "Password":"DuoS123",
+        "Port":5432,
+        "Host":"127.0.0.1",
+        "Database":"dvpdb"
+    },
+    "Types":{
+        "Type":"DEV_API"
+    },
+    "Class":{
+        "EventClass":"APP"
+    },
+
+"Debug":{
+    "ip":"127.0.0.1",
+    "port":"8088"
+},
+
+
+    "Redis":
+    {
+        "ip": "192.168.0.47",
+        "port": "6379"
+
+    },
+
+    "Host":
+    {
+        "domain": "0.0.0.0",
+        "port": "8082",
+        "version":"6.0"
+    }
+
+};
